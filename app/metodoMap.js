9b537880f6bb656f0393aba5104e17a146028840
Peter <petternds@gmail.com>
2024-07-26 06:51:15 -0300

    implementando a função filter

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,7 +1,10 @@
 function aplicarDesconto(livros) {
     const desconto = 0.3;
     livrosComDesconto = livros.map(livro => {
-        return {...livro, preco: livro.preco - (livro.preco * desconto)};
+        return {
+            ...livro,
+            preco: livro.preco - (livro.preco * desconto)
+        };
     });
     return livrosComDesconto;
 };
